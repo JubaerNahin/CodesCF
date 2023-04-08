bc1b4f5f0c1eef64bd509ad57e0edfb70a2e4a34
Nahin <jubaernahin17@gmail.com>
2023-04-09 02:04:37 +0600

    init

---
+++ 1809B.cpp
@@ -0,0 +1,14 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t,res;
+    cin>>t;
+    long long int n;
+    for(int i=0;i<t;i++){
+        cin>>n;
+        res=sqrt(n-1);
+        cout<<res<<endl;
+    }
+}
